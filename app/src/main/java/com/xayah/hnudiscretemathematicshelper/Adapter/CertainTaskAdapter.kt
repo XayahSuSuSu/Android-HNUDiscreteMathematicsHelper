6bc34f7ac596997d99ab517105d4be29b1dd5ed4
XayahSuSuSu <1249475336@qq.com>
2021-04-28 18:45:24 +0800

    1.0.0: Initialize radioButton and optimize textView

--- app/src/main/java/com/xayah/hnudiscretemathematicshelper/Adapter/CertainTaskAdapter.kt
+++ app/src/main/java/com/xayah/hnudiscretemathematicshelper/Adapter/CertainTaskAdapter.kt
@@ -4,13 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.graphics.Typeface
 import android.util.Log
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CheckBox
-import android.widget.LinearLayout
-import android.widget.TextView
+import android.widget.*
 import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
@@ -63,22 +63,35 @@ class CertainTaskAdapter(
     override fun onBindViewHolder(holder: ViewHolderTask, position: Int) {
         holder.setIsRecyclable(false) // 复用问题导致CheckBox数据错乱,搞了我好久...
         val certainTaskClass: CertainTaskClass = mCertainTaskList.get(position)
-
+        Log.d("mTAG", "答案数量: " + certainTaskClass.certainTaskQuestionClass.qAnswer)
         if (certainTaskClass.certainTaskQuestionClass.qTitle.contains("<img src=")) {
-//            holder.linearLayout_options.removeAllViews()
             val mImageUrlList =
                 DataUtil.getImageUrl(certainTaskClass.certainTaskQuestionClass.qTitle)
             holder.textView_subject.visibility = View.GONE
             for ((index, imgUrl) in mImageUrlList.withIndex()) {
                 if (index == 0 && imgUrl.isNotEmpty()) {
                     val textView = TextView(mContext)
-                    textView.setText(imgUrl)
+                    textView.text = imgUrl
                     val mParam = LinearLayout.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                        ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                    )
+                    textView.setTextColor(
+                        ContextCompat.getColor(
+                            mContext,
+                            R.color.black
+                        )
+                    )
+                    textView.setTextSize(
+                        TypedValue.COMPLEX_UNIT_PX,
+                        mContext.resources.getDimension(R.dimen.mTextViewTextSize),
+                    )
+                    textView.setTypeface(
+                        textView.typeface,
+                        Typeface.BOLD
                     )
                     mParam.setMargins(
                         mContext.resources.getDimension(R.dimen.mStart).toInt(),
-                        0,
+                        mContext.resources.getDimension(R.dimen.mTopTextView).toInt(),
                         mContext.resources.getDimension(R.dimen.mEnd).toInt(),
                         0
                     )
@@ -87,7 +100,7 @@ class CertainTaskAdapter(
                 } else
                     if (index == mImageUrlList.size - 1 && imgUrl.isNotEmpty()) {
                         val textView = TextView(mContext)
-                        textView.setText(imgUrl)
+                        textView.text = imgUrl
                         val mParam = LinearLayout.LayoutParams(
                             ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
                         )
@@ -96,6 +109,20 @@ class CertainTaskAdapter(
                             0,
                             mContext.resources.getDimension(R.dimen.mEnd).toInt(),
                             0
+                        )
+                        textView.setTextColor(
+                            ContextCompat.getColor(
+                                mContext,
+                                R.color.black
+                            )
+                        )
+                        textView.setTextSize(
+                            TypedValue.COMPLEX_UNIT_PX,
+                            mContext.resources.getDimension(R.dimen.mTextViewTextSize),
+                        )
+                        textView.setTypeface(
+                            textView.typeface,
+                            Typeface.BOLD
                         )
                         textView.layoutParams = mParam
                         holder.linearLayout_options.addView(textView)
@@ -116,13 +143,28 @@ class CertainTaskAdapter(
                             netImageView.layoutParams = mParam
                             holder.linearLayout_options.addView(netImageView)
                         }
-
                     }
             }
         } else {
             holder.textView_subject.text = certainTaskClass.certainTaskQuestionClass.qTitle
         }
 
+        if (certainTaskClass.certainTaskQuestionClass.qAnswer == "1") {
+            Log.d("mTAG", "是单选题: " + certainTaskClass.certainTaskQuestionClass.qAnswer)
+            val radioGroup = RadioGroup(mContext)
+            radioGroup.setTag("mRadioGroup")
+            val mRadioGroupParam = LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            mRadioGroupParam.setMargins(
+                0,
+                0,
+                0,
+                0
+            )
+            radioGroup.layoutParams = mRadioGroupParam
+            holder.linearLayout_options.addView(radioGroup)
+        }
 
         if (certainTaskClass.scorestudnum == "10") {
             holder.cardView.setCardBackgroundColor(
@@ -140,9 +182,49 @@ class CertainTaskAdapter(
         }
 
 
-        if (holder.linearLayout_options.childCount == 1) {
-            for ((index, i) in certainTaskClass.certainTaskQuestionClass.qOption.withIndex()) {
-                if (i.isNotEmpty()) {
+//        if (holder.linearLayout_options.childCount == 1) {
+        Log.d("mTAG", "是单选题")
+        for ((index, i) in certainTaskClass.certainTaskQuestionClass.qOption.withIndex()) {
+            if (i.isNotEmpty()) {
+                if (certainTaskClass.certainTaskQuestionClass.qAnswer == "1") {
+                    Log.d("mTAG", "是单选题")
+                    val radioButton = RadioButton(mContext)
+                    radioButton.id = View.generateViewId()
+                    radioButton.text = i
+                    if (certainTaskClass.scorestudnum == "10")
+                        radioButton.buttonTintList = ColorStateList.valueOf(Color.WHITE)
+                    if (certainTaskClass.studans.contains(DataUtil.int2Char(index)))
+                        radioButton.isChecked = true
+                    if (certainTaskClass.scorestudnum == "10") {
+                        radioButton.setTextColor(
+                            ContextCompat.getColor(
+                                mContext,
+                                R.color.white
+                            )
+                        )
+                    }
+                    radioButton.setOnCheckedChangeListener { buttonView, isChecked ->
+                        if (isChecked) {
+                            certainTaskClass.studans = DataUtil.int2Char(index).toString()
+                            mCertainTaskList[position] = certainTaskClass
+                            Log.d("mTAG", "修改后: " + certainTaskClass.studans)
+                        }
+                    }
+
+                    val mParam = LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                    )
+                    mParam.setMargins(
+                        mContext.getResources().getDimension(R.dimen.mStart).toInt(),
+                        mContext.getResources().getDimension(R.dimen.mTop).toInt(),
+                        mContext.getResources().getDimension(R.dimen.mEnd).toInt(),
+                        0
+                    )
+                    radioButton.layoutParams = mParam
+                    val mRadioGroup: RadioGroup =
+                        holder.linearLayout_options.findViewWithTag("mRadioGroup")
+                    mRadioGroup.addView(radioButton)
+                } else {
                     val checkBox = CheckBox(mContext)
                     checkBox.text = i
                     if (certainTaskClass.scorestudnum == "10")
@@ -191,6 +273,8 @@ class CertainTaskAdapter(
                     checkBox.layoutParams = mParam
                     holder.linearLayout_options.addView(checkBox)
                 }
+
+
             }
         }
     }
